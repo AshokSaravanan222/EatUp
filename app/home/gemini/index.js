b69740691e36f0f2766ef93c0c04ff80b53b3de1
Ashok Saravanan <ashko.saravanan@gmail.com>
2024-01-28 08:52:51 -0500

    api changes

--- app/home/gemini/index.js
+++ app/home/gemini/index.js
@@ -10,12 +10,15 @@ import {COLORS, SIZES, FONT} from "../../../constants"
 import Button from '../../../components/live/camera/custombutton/Button';
 
 import * as FileSystem from 'expo-file-system';
+import axios from 'axios';
 
 const API_KEY = process.env.EXPO_PUBLIC_GEMINI_API_KEY
 const genAI = new GoogleGenerativeAI(API_KEY);
 const visionModel = genAI.getGenerativeModel({ model: "gemini-pro-vision"})
 
 const prompt = "What are the ingredients displayed in this food item? Respond in the form of a string seperated by commas: Citric Acid, Soy Lecithin, ... If the item is not a food item, simply return an empty string. Only output this string, do not output anything else."
+
+const uuid = "af7c1fe6-d669-414e-b066-e9733f0de7a8"
 
 const Gemini = () => {
 
@@ -55,8 +58,7 @@ const Gemini = () => {
           const cleanedText = text.replace(/^\[|\]$/g, ''); // Removes leading and trailing brackets
           const items = cleanedText.split(',').map(item => item.trim());
           setData(items);
-
-          console.log(data);
+          console.log(items);
           setLoading(false);
         } catch (error) {
           setLoading(false);
@@ -74,6 +76,20 @@ const Gemini = () => {
     const renderItem = ({ item }) => (
     <Text style={styles.itemText}>{item}</Text>
     );
+
+    const postIngredients = async () => {
+        try {
+            console.log(data);
+          const response = await axios.post('https://xs1grhmjqd.execute-api.us-east-2.amazonaws.com/default/eatup', {
+            uuid: uuid,
+            ingredients: data, // CHANGE AFTER YOU TEST
+            // Add other necessary data or headers as required by your API
+          });
+          console.log(response.data); // Handle the response as needed
+        } catch (error) {
+          console.error('Error posting data:', error);
+        }
+      };
 
 return (
     <SafeAreaView style={styles.container}>
@@ -94,6 +110,11 @@ return (
           ListFooterComponent={<Text style={styles.footerText}>Footer</Text>}
         />
       )}
+      {
+        data.length > 0 && (
+          <Button title="Post Ingredients" onPress={postIngredients} />
+        )
+      }
     </SafeAreaView>
   );
 }
